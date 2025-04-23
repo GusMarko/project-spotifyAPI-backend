5ecd4119725ee4b490fe355b2096dd088c4439c8
Marko Gusic <markoggusic@gmail.com>
2025-04-23 07:03:35 +0200

    fix bug

--- iac/api_gateway.tf
+++ iac/api_gateway.tf
@@ -1,85 +1,193 @@
-resource "aws_api_gateway_rest_api" "spotify" {
-   name        = "spotify-api-${var.env}"
+resource "aws_api_gateway_rest_api" "spotify_api" {
+  name        = "my-spotify-api"
+  description = "API for searching Spotify artists"
 }
 
-resource "aws_api_gateway_resource" "search" {
-  rest_api_id = aws_api_gateway_rest_api.spotify.id
-  parent_id   = aws_api_gateway_rest_api.spotify.root_resource_id
+resource "aws_api_gateway_resource" "search_resource" {
+  rest_api_id = aws_api_gateway_rest_api.spotify_api.id
+  parent_id   = aws_api_gateway_rest_api.spotify_api.root_resource_id
   path_part   = "search"
 }
-resource "aws_api_gateway_method" "get" {
-  rest_api_id   = aws_api_gateway_rest_api.spotify.id
-  resource_id   = aws_api_gateway_resource.search.id
+
+resource "aws_api_gateway_method" "get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.spotify_api.id
+  resource_id   = aws_api_gateway_resource.search_resource.id
   http_method   = "GET"
   authorization = "NONE"
+  request_parameters = {
+    "method.request.querystring.artist" = true
+    "method.request.querystring.type"   = true
+  }
 }
 
-resource "aws_api_gateway_method" "options" {
-  rest_api_id   = aws_api_gateway_rest_api.spotify.id
-  resource_id   = aws_api_gateway_resource.search.id
+resource "aws_api_gateway_integration" "get_integration" {
+  rest_api_id = aws_api_gateway_rest_api.spotify_api.id
+  resource_id = aws_api_gateway_resource.search_resource.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  type        = "AWS_PROXY"
+  uri         = "arn:aws:lambda:eu-central-1:123456789012:function:spotify-search-lambda"
+  integration_http_method = "POST"
+  passthrough_behavior    = "WHEN_NO_MATCH"
+  content_handling        = "CONVERT_TO_TEXT"
+}
+
+resource "aws_api_gateway_method_response" "get_method_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.spotify_api.id
+  resource_id = aws_api_gateway_resource.search_resource.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Content-Type"                 = true
+  }
+}
+
+resource "aws_api_gateway_method_response" "get_method_response_400" {
+  rest_api_id = aws_api_gateway_rest_api.spotify_api.id
+  resource_id = aws_api_gateway_resource.search_resource.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = "400"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Content-Type"                 = true
+  }
+}
+
+resource "aws_api_gateway_method_response" "get_method_response_404" {
+  rest_api_id = aws_api_gateway_rest_api.spotify_api.id
+  resource_id = aws_api_gateway_resource.search_resource.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = "404"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Content-Type"                 = true
+  }
+}
+
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.spotify_api.id
+  resource_id   = aws_api_gateway_resource.search_resource.id
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "get_integration" {
-  rest_api_id = aws_api_gateway_rest_api.spotify.id
-  resource_id = aws_api_gateway_resource.search.id
-  http_method = aws_api_gateway_method.get.http_method
-  integration_http_method = "POST"
-  type        = "AWS_PROXY"
-  uri = aws_lambda_function.lambda.invoke_arn 
-  
-  depends_on = [ aws_api_gateway_method.get ]
-}
-
 resource "aws_api_gateway_integration" "options_integration" {
-  rest_api_id = aws_api_gateway_rest_api.spotify.id
-  resource_id = aws_api_gateway_resource.search.id
-  http_method = aws_api_gateway_method.options.http_method
+  rest_api_id = aws_api_gateway_rest_api.spotify_api.id
+  resource_id = aws_api_gateway_resource.search_resource.id
+  http_method = aws_api_gateway_method.options_method.http_method
   type        = "MOCK"
-
-   request_templates = {
+  request_templates = {
     "application/json" = "{\"statusCode\": 200}"
   }
+  passthrough_behavior = "WHEN_NO_MATCH"
 }
 
-resource "aws_api_gateway_integration_response" "options_response" {
-  rest_api_id = aws_api_gateway_rest_api.spotify.id
-  resource_id = aws_api_gateway_resource.search.id
-  http_method = aws_api_gateway_method.options.http_method
+resource "aws_api_gateway_integration_response" "options_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.spotify_api.id
+  resource_id = aws_api_gateway_resource.search_resource.id
+  http_method = aws_api_gateway_method.options_method.http_method
   status_code = "200"
-
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'"
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST'"
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Headers"  = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
   }
+  response_templates = {
+    "application/json" = ""
+  }
+  depends_on = [
+    aws_api_gateway_integration.options_integration,
+    aws_api_gateway_method.options_method,
+    aws_api_gateway_resource.search_resource
+  ]
 }
 
-resource "aws_api_gateway_method_response" "options_response" {
-  rest_api_id = aws_api_gateway_rest_api.spotify.id
-  resource_id = aws_api_gateway_resource.search.id
-  http_method = aws_api_gateway_method.options.http_method
+resource "aws_api_gateway_method_response" "options_method_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.spotify_api.id
+  resource_id = aws_api_gateway_resource.search_resource.id
+  http_method = aws_api_gateway_method.options_method.http_method
   status_code = "200"
-
-  response_models = {
-    "application/json" = "Empty"
-  }
-
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
     "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Headers"  = true
   }
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = [aws_api_gateway_integration.options_integration, aws_api_gateway_integration.get_integration]
-  rest_api_id = aws_api_gateway_rest_api.spotify.id
+  rest_api_id = aws_api_gateway_rest_api.spotify_api.id
+  stage_name  = "main"
+  depends_on = [
+    aws_api_gateway_integration.get_integration,
+    aws_api_gateway_method_response.get_method_response_200,
+    aws_api_gateway_method_response.get_method_response_400,
+    aws_api_gateway_method_response.get_method_response_404,
+    aws_api_gateway_integration_response.options_response_200,
+    aws_api_gateway_method_response.options_method_response_200,
+    aws_api_gateway_method.options_method,
+    aws_api_gateway_method.get_method
+  ]
 }
 
-resource "aws_api_gateway_stage" "stage" {
-  rest_api_id = aws_api_gateway_rest_api.spotify.id
-  deployment_id = aws_api_gateway_deployment.deployment.id
-  stage_name = "${var.env}"
+resource "aws_api_gateway_integration_response" "get_integration_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.spotify_api.id
+  resource_id = aws_api_gateway_resource.search_resource.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = aws_api_gateway_method_response.get_method_response_200.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Headers"  = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Content-Type"                 = "'application/json'"
+  }
+  response_templates = {
+    "application/json" = "$input.json('$')"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "get_integration_response_400" {
+  rest_api_id = aws_api_gateway_rest_api.spotify_api.id
+  resource_id = aws_api_gateway_resource.search_resource.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = aws_api_gateway_method_response.get_method_response_400.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Headers"  = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Content-Type"                 = "'application/json'"
+  }
+  response_templates = {
+    "application/json" = "$input.json('$')"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "get_integration_response_404" {
+  rest_api_id = aws_api_gateway_rest_api.spotify_api.id
+  resource_id = aws_api_gateway_resource.search_resource.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = aws_api_gateway_method_response.get_method_response_404.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Headers"  = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Content-Type"                 = "'application/json'"
+  }
+  response_templates = {
+    "application/json" = "$input.json('$')"
+  }
+}
+
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.spotify_api.execution_arn}/*/GET/search"
 }
